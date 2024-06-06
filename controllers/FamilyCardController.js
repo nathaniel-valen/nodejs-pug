eb2055a441e8a1400970e853c8097bab510c1217
Valen R <92865172+nathaniel-valen@users.noreply.github.com>
2024-06-06 10:18:16 +0700

    first commit

--- controllers/FamilyCardController.js
+++ controllers/FamilyCardController.js
@@ -1,5 +1,14 @@
+const familyCards = [
+    {id: '00001', name: 'John Doe'},
+    {id: '00002', name: 'John Doi'},
+    {id: '00003', name: 'John Dou'},
+
+]
+
 const index = (req, res) => {
-    res.render('family_card/index')
+    res.render('family_card/index', {
+        familyCards: familyCards
+    })
 }
 
 module.exports = { index }
